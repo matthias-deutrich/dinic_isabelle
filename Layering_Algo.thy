15ca170e4508eabf29d0354dc5a838e482c52ea4
matthias <matthias-deutrich@t-online.de>
2024-02-28 13:13:43 +0100

    Progress on ebfs phase correctness proof

--- Layering_Algo.thy
+++ Layering_Algo.thy
@@ -17,6 +17,11 @@ text \<open>In this section, we present an extended version of breadth-first sea
 
 (* TODO make the successor function parametric or enable inverting graphs*)
 definition (in Graph) inverted where "inverted \<equiv> c \<circ> prod.swap"
+
+thm swap_swap
+lemma inverted_inverted[simp]: "Graph.inverted (Graph.inverted c) = c"
+  unfolding Graph.inverted_def by fastforce
+
 find_consts "('a \<times> 'b \<Rightarrow> 'c) \<Rightarrow> ('b \<times> 'a) \<Rightarrow> 'c"
 
 (* TODO check whether such a thing exists *)
@@ -39,29 +44,78 @@ definition ebfs_phase :: "_ graph \<Rightarrow> node set \<Rightarrow> (_ graph 
 
 definition ebfs_phase_invar :: "node \<Rightarrow> nat \<Rightarrow> node set \<Rightarrow> (_ graph \<times> node set) \<Rightarrow> bool" where
   "ebfs_phase_invar s n Q \<equiv> \<lambda>(c', Q').
-    S_Shortest_Path_Union c' c s (boundedReachableNodes (n + 1) s - Q)
-    \<and> True" (* TODO Q' property *)
+    S_Shortest_Path_Union c' c s (boundedReachableNodes (Suc n) s - Q)
+    \<and> Q' = exactDistNodes (n + 2) s \<inter> {v. \<exists>u \<in> Graph.V c'. (u, v) \<in> E}"
+
+lemma ebfs_phase_initial:
+  assumes "S_Shortest_Path_Union c' c s (boundedReachableNodes n s)"
+  shows "ebfs_phase_invar s n (exactDistNodes (Suc n) s) (c', {})"
+  unfolding ebfs_phase_invar_def
+proof (intro case_prodI conjI)
+  show "S_Shortest_Path_Union c' c s (boundedReachableNodes (Suc n) s - exactDistNodes (Suc n) s)"
+    using assms by (simp add: ebfs_phase_invar_def exactDistNodes_alt boundedReachableNodes_mono double_diff)
+
+  have "\<And>v. \<exists>u \<in> Graph.V c'. (u, v) \<in> E \<Longrightarrow> v \<notin> exactDistNodes (n + 2) s"
+  proof -
+    interpret S_Shortest_Path_Union c' c s "boundedReachableNodes n s" using assms .
+
+    fix v
+    assume "\<exists>u \<in> Graph.V c'. (u, v) \<in> E"
+    then obtain u where "u \<in> Graph.V c'" "(u, v) \<in> E" by blast
+    then obtain t p\<^sub>s p\<^sub>t where "t \<in> boundedReachableNodes n s" "isShortestPath s (p\<^sub>s @ p\<^sub>t) t"
+      and P2: "isShortestPath s p\<^sub>s u"
+      by (blast elim: obtain_shortest_ST_paths)
+    then have "length p\<^sub>s \<le> n" unfolding boundedReachableNodes_def isShortestPath_min_dist_def by simp
+    with P2 \<open>(u, v) \<in> E\<close> have "min_dist s v \<le> Suc n"
+      using isShortestPath_min_dist_def connected_def min_dist_succ by fastforce
+    then show "v \<notin> exactDistNodes (n + 2) s" unfolding exactDistNodes_def by simp
+  qed
+  then show "{} = exactDistNodes (n + 2) s \<inter> {v. \<exists>u\<in>Graph.V c'. (u, v) \<in> E}" by blast
+qed
+
+lemma ebfs_phase_final: (* TODO switch order *)
+  assumes "ebfs_phase_invar s n {} (c', Q')"
+  shows "S_Shortest_Path_Union c' c s (boundedReachableNodes (Suc n) s) \<and> Q' = exactDistNodes (n + 2) s"
+proof
+  from assms show "S_Shortest_Path_Union c' c s (boundedReachableNodes (Suc n) s)"
+    unfolding ebfs_phase_invar_def by simp
+  then interpret S_Shortest_Path_Union c' c s "boundedReachableNodes (Suc n) s" . (* TODO necessary? *)
+
+  from assms have "Q' = exactDistNodes (n + 2) s \<inter> {v. \<exists>u \<in> Graph.V c'. (u, v) \<in> E}"
+    unfolding ebfs_phase_invar_def by simp
+  moreover have "exactDistNodes (n + 2) s \<subseteq> {v. \<exists>u \<in> Graph.V c'. (u, v) \<in> E}"
+  proof (* TODO prettify *)
+    fix v
+    assume "v \<in> exactDistNodes (n + 2) s"
+    then obtain p where P: "isShortestPath s p v" "length p = n + 2" unfolding exactDistNodes_def
+      by (metis (mono_tags) isShortestPath_min_dist_def mem_Collect_eq obtain_shortest_path)
+    then obtain p\<^sub>u u where "p = p\<^sub>u @ [(u, v)]"
+      by (metis Graph.isPath_bwd_cases add_2_eq_Suc' isShortestPath_def length_Suc_conv list.distinct(1))
+    with P have "isShortestPath s p\<^sub>u u" "length p\<^sub>u = n + 1" "(u, v) \<in> E"
+      using split_shortest_path_around_edge 
+      by (metis Graph.isPath_tail add_Suc_right add_left_imp_eq length_append_singleton nat_1_add_1 plus_1_eq_Suc shortestPath_is_path)+
+    then have "u \<in> boundedReachableNodes (Suc n) s" unfolding boundedReachableNodes_def
+      by (metis connected_def Suc_eq_plus1 isShortestPath_def isShortestPath_min_dist_def mem_Collect_eq)
+    then have "u \<in> V'"
+      by (metis Graph.connected_edgeRtc Graph.connected_inV_iff Graph.distinct_nodes_in_V_if_connected(1) Graph.isPath_rtc Graph.isShortestPath_alt Graph.shortestPath_is_path Graph.simplePath_same_conv Suc_eq_plus1 \<open>isShortestPath s p\<^sub>u u\<close> \<open>length p\<^sub>u = n + 1\<close> insert_iff length_Suc_conv list.distinct(1) shortest_ST_path_remains)
+    with \<open>(u, v) \<in> E\<close> show "v \<in> {v. \<exists>u\<in>V'. (u, v) \<in> E}" by blast
+  qed
+  ultimately show "Q' = exactDistNodes (n + 2) s" by blast
+qed
 
 lemma ebfs_phase_step:
   assumes "u \<in> Q" and "Q \<subseteq> exactDistNodes (Suc n) s" and "ebfs_phase_invar s n Q (c', Q')"
   shows "ebfs_node u c' Q' \<le> SPEC (ebfs_phase_invar s n (Q - {u}))"
   sorry
 
-lemma ebfs_phase_final:
-  assumes INVAR: "ebfs_phase_invar s "
-
-lemma ebfs_phase_final: oops
-
 lemma ebfs_phase_correct:
   fixes s c' Q n
-  assumes "S_Shortest_Path_Union c' c s (boundedReachableNodes n s)"
-    and "finite (exactDistNodes (Suc n) s)"
+  assumes "finite (exactDistNodes (Suc n) s)"
+    and "S_Shortest_Path_Union c' c s (boundedReachableNodes n s)"
   shows "ebfs_phase c' (exactDistNodes (Suc n) s) \<le> SPEC(\<lambda>(c'', Q'). S_Shortest_Path_Union c'' c s (boundedReachableNodes (Suc n) s) \<and> Q' = exactDistNodes (n + 2) s)"
   unfolding ebfs_phase_def
   apply (refine_vcg FOREACH_rule[where I="ebfs_phase_invar s n"])
-  using assms apply blast
-  using assms apply (simp add: ebfs_phase_invar_def exactDistNodes_alt boundedReachableNodes_mono double_diff)
-  using ebfs_phase_step apply simp
+  using assms ebfs_phase_initial ebfs_phase_step ebfs_phase_final by simp_all
 
 thm FOREACH_rule
 (* TODO somewhere we need the fact that the set of reachable nodes (from s) is finite *)
